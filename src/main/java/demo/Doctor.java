11b396f52dd81f9f8b03ec1812a043fd77a52ab8
Arman Islam <armaanislam28@gmail.com>
2022-07-31 02:12:37 +0600

    Annotation Configuration

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -1,32 +1,12 @@
 package demo;
 
+import org.springframework.stereotype.Component;
+
+@Component // To use annotations, we have to mark this as a spring component
 public class Doctor implements Staff {
-
-    private String qualification;
-    //    private Nurse nurse;
-
-    public Doctor(String qualification) {
-        this.qualification = qualification;
-    }
 
     public void assist() {
         System.out.println("Doctor is assisting");
-    }
-
-    public String getQualification() {
-        return qualification;
-    }
-
-//    public Nurse getNurse() {
-//        return nurse;
-//    }
-//
-//    public void setNurse(Nurse nurse) {
-//        this.nurse = nurse;
-//    }
-
-    public void setQualification(String qualification) {
-        this.qualification = qualification;
     }
 
 }
--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -9,12 +9,8 @@ public class Main {
     public static void main(String[] args) {
 
     ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-    Doctor staff = context.getBean(Doctor.class);
+    Staff staff = context.getBean(Doctor.class);
     staff.assist();
-    System.out.println(staff.getQualification());
-//    staff = (Nurse) context.getBean("nurse"); // We can also get a class by its id, but we need to type cast it by the class name
-//    staff.assist();
 
     }
 }
- 
