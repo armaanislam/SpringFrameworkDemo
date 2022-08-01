90e5d08ef6663530239dea4fab9a06affd6ed074
MD. Arman Islam <armaanislam84@gmail.com>
2022-08-01 10:15:37 +0600

    Java Configuration

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 @Component // To use annotations, we have to mark this as a spring component
 public class Doctor implements Staff {
 
+    public Doctor() {
+    }
+
     public void assist() {
         System.out.println("Doctor is assisting");
     }
